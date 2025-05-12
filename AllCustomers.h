a87be4da2fb8e8a01f252fce34e488a8c0b93821
Daniel Voyevoda <danielvoyevoda@outlook.com>
2025-05-11 20:34:05 -0400

    Set up main files and created input data

---
+++ AllCustomers.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+
+class AllCustomers {
+    public:
+        
+    private:
+        std::string firstName, lastName, streetAddress, city, state;
+        int accountNumber, zipCode, phoneNumber;
+};
---
+++ AllPurchases.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+class AllPurchases {
+    public:
+        
+    private:
+        std::string item, date;
+        int accountNumber;
+        double ammount;
+};
